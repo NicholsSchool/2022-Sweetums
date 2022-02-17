34f6fbcd87ca68304d543fdf749f87fbf2c01af5
Eleanor Bares <eleanorbares@icloud.com>
2022-02-17 17:45:44 -0500

    added changes

--- src/main/java/frc/robot/subsystems/Shifter.java
+++ src/main/java/frc/robot/subsystems/Shifter.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 import edu.wpi.first.wpilibj.Solenoid;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.RobotMap;
 
 public class Shifter extends SubsystemBase 
@@ -12,18 +15,21 @@ public class Shifter extends SubsystemBase
     public Shifter()
     {
         shifter = new Solenoid( PneumaticsModuleType.CTREPCM, RobotMap.SHIFTER_SOLENOID_CHANNEL );
+
         shifter.set( true );
     }
 
     public void highGear()
     {
         shifter.set( true );
+
         RobotMap.GOVERNOR = RobotMap.GOVERNOR_FAST;
     }
 
     public void lowGear()
     {
         shifter.set( false );
+
         RobotMap.GOVERNOR = RobotMap.GOVERNOR_SLOW;
     }
 }
