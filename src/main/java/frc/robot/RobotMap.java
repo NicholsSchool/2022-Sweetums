589ff7fecdd8d1f71244312f7481595fedc2714e
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2022-02-21 22:47:27 -0500

    ID'd

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,27 +2,28 @@ package frc.robot;
 
 public class RobotMap 
 {
-    public static final int RIGHT_FRONT_DRIVE_ID = 10;
-    public static final int RIGHT_BACK_DRIVE_ID = 12;
-    public static final int LEFT_FRONT_DRIVE_ID = 11;
-    public static final int LEFT_BACK_DRIVE_ID = 13;
+    public static final int RIGHT_FRONT_DRIVE_ID = 18;
+    public static final int RIGHT_BACK_DRIVE_ID = 19;
+    public static final int LEFT_FRONT_DRIVE_ID = 16;
+    public static final int LEFT_BACK_DRIVE_ID = 17;
+    
+    public static final int SHOOTER_ID = 25;
 
-    public static final int INTAKE_ROLLER_ID = 22;
-    public static final int SORTER_ID = 23;
+    public static final int INDEXER_ID = 42;
+
+    public static final int RIGHT_CLIBER_ID = 47;
+    public static final int LEFT_CLIBER_ID = 48;
+
+    public static final int RIGHT_INTAKE_ID = 45;
+    public static final int LEFT_INTAKE_ID = 44;
 
     public static final int PCM_CAN_ID = 51; // don't have the correct ID
 
     public static final double GOVERNOR_FAST = 1.0;
     public static final double GOVERNOR_SLOW = 0.7;
     public static double GOVERNOR = GOVERNOR_SLOW;
-
-    public static final int SHOOTER_ID = 20;
     
-    public static final int INDEXER_ID = 21;
-    
-    public static final int SHIFTER_SOLENOID_CHANNEL = 0;
+    public static final int SHIFTER_SOLENOID_CHANNEL = 0; // May be 1
 
-    public static final int PISTON_SOLENOID_CHANNEL = 1;
-
-    public static final int SLIDER_SOLENOID_CHANNEL = 2;
+    public static final int SLIDER_SOLENOID_CHANNEL = 1; // May be 0
 }
